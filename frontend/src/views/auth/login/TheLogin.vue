b184bc171c8ae56ce0ed66ed7a1a3836ee455f71
tuanct413 <tuanct413@gmail.com>
2025-06-23 16:04:30 +0700

    fix img-url-logo

--- frontend/src/views/auth/login/TheLogin.vue
+++ frontend/src/views/auth/login/TheLogin.vue
@@ -5,7 +5,9 @@
   </template>
   
 <script setup>
-  import loginBg from '@/assets/img/login/login.jpg'
+  import loginBg from 'src/assets/img/login/login.jpg';
+  const logoUrl = @/assets/img/logo/logo_tab_oboe.svg ;
+  C:\Users\tuanc\OneDrive\Desktop\OboeProject\frontend\src\assets\img\login\login.jpg
   import '@/views/auth/login/TheLogin.scss';
   import FormAuthen from '@/components/layout/form-login/FormAuthen.vue';
 
