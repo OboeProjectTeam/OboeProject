d6c17f83fbffe50cd301e00b936d5ee07d119d20
hoangdu999 <hoangdu1999s2@gmail.com>
2025-07-13 17:56:09 +0700

    giai quyet xung dot

--- frontend/src/components/layout/flashcard-list/FlashcardList.vue
+++ frontend/src/components/layout/flashcard-list/FlashcardList.vue
@@ -10,8 +10,6 @@
       <i class="fas fa-book"></i>
       <span v-if="totalItems > 0" class="item-count">{{ totalItems }}</span>
     </button>
-
-    <!-- Dropdown List -->
     <div v-if="isOpen" class="flashcard-list">
       <div class="list-header">
         <h3>Danh sách Flashcard</h3>
