4e2260fff8ea8ac6b49af4c2fe1fb245564b92a1
hoangdu999 <hoangdu1999s2@gmail.com>
2025-07-13 17:05:02 +0700

    back to code

--- frontend/src/components/layout/flashcard-list/FlashcardList.vue
+++ frontend/src/components/layout/flashcard-list/FlashcardList.vue
@@ -71,7 +71,6 @@ const isOpen = ref(false)
 const activeTab = ref('word')
 const isDragging = ref(false)
 
-// Style binding for container position
 const containerStyle = computed(() => ({
   transform: `translateY(${position.value.y}px)`
 }))
