a7c63a00d5bdacd93b394f93ef8f2a8ecf7cccd7
hoangdu999 <hoangdu1999s2@gmail.com>
2025-07-17 20:54:53 +0700

    redirect sau khi đăng nhập OAuth2

--- backend/Oboe/src/main/java/com/example/Oboe/Config/SpaFallbackConfig.java
+++ backend/Oboe/src/main/java/com/example/Oboe/Config/SpaFallbackConfig.java
@@ -12,9 +12,11 @@ public class SpaFallbackConfig implements WebMvcConfigurer {
         // Fallback route để tất cả request frontend đều trả về index.html
         registry.addViewController("/{spring:[a-zA-Z0-9-_]+}")
                 .setViewName("forward:/index.html");
-        registry.addViewController("/**/{spring:[a-zA-Z0-9-_]+}")
-                .setViewName("forward:/index.html");
         registry.addViewController("/{spring:[a-zA-Z0-9-_]+}/**{spring:?!(\\.js|\\.css|\\.png|\\.jpg|\\.jpeg|\\.svg|\\.woff|\\.ttf)$}")
+                .setViewName("forward:/index.html");
+
+        //  hỗ trợ redirect sau khi đăng nhập OAuth2
+        registry.addViewController("/oauth2/redirect")
                 .setViewName("forward:/index.html");
     }
 }
