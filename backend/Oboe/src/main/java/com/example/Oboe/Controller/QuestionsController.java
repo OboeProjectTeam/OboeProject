ad042460553909dbeae4ee8ae611c6be84aeee49
tuanct413 <tuanct413@gmail.com>
2025-07-24 15:03:24 +0700

    sửa lại add question thêm dc nhiều câu hỏi và get ra phân trang

--- backend/Oboe/src/main/java/com/example/Oboe/Controller/QuestionsController.java
+++ backend/Oboe/src/main/java/com/example/Oboe/Controller/QuestionsController.java
@@ -3,9 +3,15 @@ package com.example.Oboe.Controller;
 import com.example.Oboe.DTOs.QuestionDTO;
 import com.example.Oboe.Service.QuestionsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -16,12 +22,35 @@ public class QuestionsController {
     private QuestionsService questionsService;
 
     @PostMapping
-    public QuestionDTO create(@RequestBody QuestionDTO dto) {
-        return questionsService.create(dto);
+    public ResponseEntity<Map<String, Object>> create(@RequestBody List<QuestionDTO> dtoList) {
+        List<QuestionDTO> created = questionsService.create(dtoList);
+        Map<String, Object> response = new HashMap<>();
+        response.put("thông báo", "thêm câu thành công");
+        response.put("data",created);
+        response.put("số lượng câu hỏi đã thêm",created.size());
+        return ResponseEntity.ok(response);
+
+    }
+    //// API lấy danh sách câu hỏi theo quizId, có hỗ trợ phân trang
+    @GetMapping("/by-quiz/{quizId}")
+    public ResponseEntity<Map<String, Object>> getByQuizPaged(
+            @PathVariable UUID quizId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "8") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<QuestionDTO> pageResult = questionsService.getQuestionsByQuizId(quizId, pageable);
+
+        /// Tạo map để cấu trúc lại dữ liệu phân trang trả về cho dữ liệu đẹp
+        Map<String, Object> response = new HashMap<>();
+        response.put("thông báo", "lấy ra toàn bộ câu hỏi thành công");
+        response.put("content", pageResult.getContent());
+        response.put("pageNumber", pageResult.getNumber());
+        response.put("pageSize", pageResult.getSize());
+        response.put("totalElements", pageResult.getTotalElements());
+        response.put("totalPages", pageResult.getTotalPages());
+        response.put("last", pageResult.isLast());
+        return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/by-quiz/{quizId}")
-    public List<QuestionDTO> getByQuiz(@PathVariable UUID quizId) {
-        return questionsService.getQuestionsByQuizId(quizId);
-    }
 }
