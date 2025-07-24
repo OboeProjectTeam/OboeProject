c72209df3dbcc44b432465aafd4af5d2aa1ef890
hoangdu999 <hoangdu1999s2@gmail.com>
2025-07-25 01:26:50 +0700

    sửa format backend nghia

--- backend/Oboe/src/main/java/com/example/Oboe/Controller/FlashCardController.java
+++ backend/Oboe/src/main/java/com/example/Oboe/Controller/FlashCardController.java
@@ -68,14 +68,12 @@
         }
 
 
-
-
+        @GetMapping("/latest")
+        public ResponseEntity<?> getTop5LatestFlashCards(
+                @AuthenticationPrincipal(expression = "userID") UUID userId) {
+            return ResponseEntity.ok(flashCardService.getTop5LatestFlashCards(userId));
+        }
     }
 
-    @GetMapping("/latest")
-    public ResponseEntity<?> getTop5LatestFlashCards(
-            @AuthenticationPrincipal(expression = "userID") UUID userId) {
-        return ResponseEntity.ok(flashCardService.getTop5LatestFlashCards(userId));
-    }
 
-}
+
