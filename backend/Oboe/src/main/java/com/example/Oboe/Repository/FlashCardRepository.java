8fc9a0c505f3ecc2737ad76538240cdfef7a34ee
hoangdu999 <hoangdu1999s2@gmail.com>
2025-07-25 07:53:49 +0700

    rename countFlashCardByUserId

--- backend/Oboe/src/main/java/com/example/Oboe/Repository/FlashCardRepository.java
+++ backend/Oboe/src/main/java/com/example/Oboe/Repository/FlashCardRepository.java
@@ -30,5 +30,5 @@ public interface FlashCardRepository extends JpaRepository<FlashCards, UUID> {
                                            @Param("term") String term,
                                            Pageable pageable);
     @Query("SELECT f FROM FlashCards f WHERE f.user.user_id = :userId ORDER BY f.created DESC")
-    List<FlashCards> findTop5ByUserIdOrderByCreatedDesc(@Param("userId") UUID userId);
+    long countFlashCardByUserId(@Param("userId") UUID userId);
 }
