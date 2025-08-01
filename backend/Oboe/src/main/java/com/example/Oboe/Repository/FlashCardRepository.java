339efa671ca9343e947e3df146bcf45740357d90
wuong161104 <thanhvuong161104@gmail.com>
2025-08-01 23:30:46 +0700

    Update ReplyBog and Generate

--- backend/Oboe/src/main/java/com/example/Oboe/Repository/FlashCardRepository.java
+++ backend/Oboe/src/main/java/com/example/Oboe/Repository/FlashCardRepository.java
@@ -46,6 +46,18 @@ public interface FlashCardRepository extends JpaRepository<FlashCards, UUID> {
     @Query("SELECT f FROM FlashCards f WHERE f.set_id = :setId")
     Optional<FlashCards> findById(@Param("setId") UUID setId);
 
+    @Query(
+            value = "SELECT new com.example.Oboe.DTOs.FlashcardSearchResultDTO(" +
+                    "f.set_id, f.description, f.user.userName, COUNT(c), f.user.avatarUrl) " +
+                    "FROM FlashCards f LEFT JOIN f.cardItems c " +
+                    "WHERE LOWER(f.description) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+                    "GROUP BY f.set_id, f.description, f.user.userName, f.user.avatarUrl",
+            countQuery = "SELECT COUNT(DISTINCT f.set_id) " +
+                    "FROM FlashCards f " +
+                    "WHERE LOWER(f.description) LIKE LOWER(CONCAT('%', :keyword, '%'))"
+    )
+    Page<FlashcardSearchResultDTO> searchFlashcardsByKeyword(@Param("keyword") String keyword, Pageable pageable);
+
 
 
 }
